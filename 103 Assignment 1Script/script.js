6fdff47b0b70d1db48fb12053b888c43a4fd2632
justinemrivera <87049200+justinemrivera@users.noreply.github.com>
2021-07-26 14:20:07 -0700

    Add files via upload

---
+++ 103 Assignment 1Script/script.js
@@ -0,0 +1,88 @@
+
+var userName= "Christian Stewart";
+var userAge= 4;
+
+document.write(`
+<p> <b> Child's Name: </b> ${userName}</p>
+<p> <b> Age: </b> ${userAge}</p>
+`);
+
+var DOB="11/24/2016"
+var emergencyContact="619-598-5989"
+
+document.write(`
+<p> <b> Child's DOB: </b> ${DOB}</p>
+<p> <b> Emergency Contact on File: </b> ${emergencyContact}</p>
+`);
+
+var gaurdian="Dave Stewart";
+
+document.write(`
+<p> <b> Parent/Guardian: </b> ${gaurdian}</p>
+`);
+
+var gaurdian="Justine Rivera";
+document.write(`
+<p> <b> Parent/Guardian: </b> ${gaurdian}</p>
+`);
+
+var coach="Coach Thomas";
+var assistantCoach="Assistant Coach Ryan";
+var gameLocation="Canyon Steele Park";
+var fieldNumber=1;
+
+document.write(`
+<p> <b> Coach: </b> ${coach}</p>
+<p> <b> Assistant Coach: </b> ${assistantCoach}</p>
+<p> <b> Location: </b> ${gameLocation}</p>
+<p> <b> Field #: </b> ${fieldNumber}</p>
+`);
+
+var startDate= "August 10";
+var endDate= "September 10"
+
+
+document.write(`
+<p> <b> Start Date: </b> ${startDate}</p>
+<p> <b> End Date: </b> ${endDate}</p>
+`);
+
+var practiceTime= "10am-11am";
+var gameTime= "11am-12:30pm"
+
+
+document.write(`
+<p> <b> Practice Time: </b> ${practiceTime}</p>
+<p> <b> Game Time: </b> ${gameTime}</p>
+`);
+
+
+var uniformSize="5t";
+var uniformTotal="$50";
+
+document.write(`
+<p> <b> Uniform Size: </b> ${uniformSize}</p>
+<p> <b> Uniform Total: </b> ${uniformTotal}</p>
+`);
+
+var programCost="$100";
+var totalCost="$150";
+
+document.write(`
+<p> <b> Program Cost: </b> ${programCost}</p>
+<p> <b> Total Cost: </b> ${totalCost}</p>
+`);
+
+var enrollmentDate="7/20/2021";
+var confirmation="CD45678";
+
+document.write(`
+<p> <b> Enrollment Date: </b> ${enrollmentDate}</p>
+<p> <b> Confirmation #: </b> ${confirmation}</p>
+`);
+
+
+
+
+
+
